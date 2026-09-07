ff833ce38116052eb2167cb65e8d8225db7f4b04
SENKU <senku524895985@gmail.com>
2026-09-07 22:10:19 +0800

    Remove standalone tests directory

--- tests/ir_receive_support/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef IR_RECEIVE_TEST_PLATFORM_H
-#define IR_RECEIVE_TEST_PLATFORM_H
-#include <stdint.h>
-extern uint32_t test_primask;
-static inline uint32_t __get_PRIMASK(void) { return test_primask; }
-static inline void __disable_irq(void) { test_primask = 1U; }
-static inline void __set_PRIMASK(uint32_t value) { test_primask = value; }
-#endif
